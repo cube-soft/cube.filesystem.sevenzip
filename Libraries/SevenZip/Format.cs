1e7936f48fbd6ab49df973a9f18ceffd332048de
clown <tt.clown@gmail.com>
2017-06-13 20:07:52 +0900

    Add some formats

--- Libraries/SevenZip/Format.cs
+++ Libraries/SevenZip/Format.cs
@@ -33,26 +33,39 @@ namespace Cube.FileSystem.SevenZip
     public enum Format
     {
         Unknown     = -1,
-        SevenZip    =  0,
-        Arj,
-        BZip2,
-        Cab,
-        Chm,
-        Compound,
-        Cpio,
-        Deb,
-        GZip,
-        Iso,
-        Lzh,
-        Lzma,
-        Nsis,
-        Rar,
-        Rpm,
-        Split,
-        Tar,
-        Wim,
-        Z,
-        Zip
+        SevenZip    =  0,   // Open 7-zip archive format.
+        Arj,                // Proprietary Arj archive format.
+        BZip2,              // Open Bzip2 archive format.
+        Cab,                // Microsoft cabinet archive format.
+        Chm,                // Microsoft Compiled HTML Help file format.
+        Compound,           // Microsoft Compound file format.
+        Cpio,               // Open Debian software package format.
+        Deb,                // Open Gzip archive format.
+        Dmg,                // Apple Mac OS X Disk Copy Disk Image format.
+        Elf,                // Linux executable Elf format.
+        Flv,                // Flash video format.
+        GZip,               // Open Gzip archive format.
+        Hfs,                // Macintosh Disk Image on CD.
+        Iso,                // Open ISO disk image format.
+        Lzh,                // Open Lzh archive format.
+        Lzma,               // Open core 7-zip Lzma raw archive format.
+        Lzw,                // Open LZW archive format (a.k.a "Z" archive format).
+        Mub,                // Mub
+        Msi,                // Windows Installer Database.
+        Nsis,               // Nullsoft installation package format.
+        Mslz,               // MSLZ archive format.
+        PE,                 // Windows PE executable format.
+        Rar,                // RarLab Rar archive format.
+        Rpm,                // Open Rpm software package format.
+        Split,              // Open split file format.
+        Swf,                // Shockwave Flash format.
+        Tar,                // Open Tar archive format.
+        Udf,                // Open Udf disk image format.
+        Vhd,                // Microsoft virtual hard disk file format.
+        Wim,                // Microsoft Windows Imaging disk image format.
+        Xar,                // Xar open source archive format.
+        XZ,                 // Open XZ archive format.
+        Zip,                // Open Zip archive format.
     }
 
     /* --------------------------------------------------------------------- */
@@ -66,6 +79,8 @@ namespace Cube.FileSystem.SevenZip
     /* --------------------------------------------------------------------- */
     public static class FormatConversions
     {
+        #region Methods
+
         /* ----------------------------------------------------------------- */
         ///
         /// ToClassId
@@ -94,19 +109,30 @@ namespace Cube.FileSystem.SevenZip
                     { Format.Compound,  new Guid("23170f69-40c1-278a-1000-000110e50000") },
                     { Format.Cpio,      new Guid("23170f69-40c1-278a-1000-000110ed0000") },
                     { Format.Deb,       new Guid("23170f69-40c1-278a-1000-000110ec0000") },
+                    { Format.Dmg,       new Guid("23170f69-40c1-278a-1000-000110E40000") },
+                    { Format.Elf,       new Guid("23170f69-40c1-278a-1000-000110DE0000") },
                     { Format.GZip,      new Guid("23170f69-40c1-278a-1000-000110ef0000") },
+                    { Format.Hfs,       new Guid("23170f69-40c1-278a-1000-000110E30000") },
                     { Format.Iso,       new Guid("23170f69-40c1-278a-1000-000110e70000") },
                     { Format.Lzh,       new Guid("23170f69-40c1-278a-1000-000110060000") },
                     { Format.Lzma,      new Guid("23170f69-40c1-278a-1000-0001100a0000") },
+                    { Format.Lzw,       new Guid("23170f69-40c1-278a-1000-000110050000") },
+                    { Format.Mub,       new Guid("23170f69-40c1-278a-1000-000110E20000") },
+                    { Format.Mslz,      new Guid("23170f69-40c1-278a-1000-000110D50000") },
                     { Format.Nsis,      new Guid("23170f69-40c1-278a-1000-000110090000") },
+                    { Format.PE,        new Guid("23170f69-40c1-278a-1000-000110DD0000") },
                     { Format.Rar,       new Guid("23170f69-40c1-278a-1000-000110030000") },
                     { Format.Rpm,       new Guid("23170f69-40c1-278a-1000-000110eb0000") },
                     { Format.Split,     new Guid("23170f69-40c1-278a-1000-000110ea0000") },
+                    { Format.Swf,       new Guid("23170f69-40c1-278a-1000-000110D70000") },
                     { Format.Tar,       new Guid("23170f69-40c1-278a-1000-000110ee0000") },
+                    { Format.Udf,       new Guid("23170f69-40c1-278a-1000-000110E00000") },
+                    { Format.Vhd,       new Guid("23170f69-40c1-278a-1000-000110DC0000") },
                     { Format.Wim,       new Guid("23170f69-40c1-278a-1000-000110e60000") },
-                    { Format.Z,         new Guid("23170f69-40c1-278a-1000-000110050000") },
+                    { Format.Xar,       new Guid("23170f69-40c1-278a-1000-000110E10000") },
+                    { Format.XZ,        new Guid("23170f69-40c1-278a-1000-0001100C0000") },
                     { Format.Zip,       new Guid("23170f69-40c1-278a-1000-000110010000") },
-                };
+               };
             }
             return _guid.ContainsKey(fmt) ? _guid[fmt] : Guid.Empty;
         }
@@ -140,6 +166,24 @@ namespace Cube.FileSystem.SevenZip
             var cvt = ext.ToLower();
             return _ext.ContainsKey(cvt) ? _ext[cvt] : Format.Unknown;
         }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// FromFileName
+        ///
+        /// <summary>
+        /// ファイル名に対応する Format を取得します。
+        /// </summary>
+        /// 
+        /// <param name="path">ファイル名</param>
+        /// 
+        /// <returns>Format オブジェクト</returns>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static Format FromFileName(string path)
+            => FromExtension(System.IO.Path.GetExtension(path));
+
+        #endregion
 
         #region Fields
         private static IDictionary<string, Format> _ext;
