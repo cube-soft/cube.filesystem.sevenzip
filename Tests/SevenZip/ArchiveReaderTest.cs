cb0128bf074419555deff0a8288d82c7b73a0ecf
clown <tt.clown@gmail.com>
2017-06-13 20:13:42 +0900

    Update tests

---
+++ Tests/SevenZip/ArchiveReaderTest.cs
@@ -0,0 +1,62 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// Copyright (c) 2010 CubeSoft, Inc.
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Lesser General Public License as
+/// published by the Free Software Foundation, either version 3 of the
+/// License, or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Lesser General Public License for more details.
+///
+/// You should have received a copy of the GNU Lesser General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///
+/* ------------------------------------------------------------------------- */
+using System.Linq;
+using NUnit.Framework;
+
+
+namespace Cube.FileSystem.Tests
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// ArchiveReaderTest
+    /// 
+    /// <summary>
+    /// ArchiveReader のテスト用クラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [Parallelizable]
+    [TestFixture]
+    class ArchiveReaderTest : FileResource
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Items
+        ///
+        /// <summary>
+        /// 圧縮ファイルのリストを取得するテストを実行します。
+        /// </summary>
+        /// 
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void Items()
+        {
+            using (var archive = new SevenZip.ArchiveReader())
+            {
+                archive.Open(Example("Sample.zip"), string.Empty);
+
+                var actual = archive.Items.ToList();
+                Assert.That(actual.Count,   Is.EqualTo(3));
+                Assert.That(actual[2].Path, Is.EqualTo("Foo.txt"));
+                Assert.That(actual[0].Path, Is.EqualTo("Bar.txt"));
+                Assert.That(actual[1].Path, Is.EqualTo("Bas.txt"));
+            }
+        }
+    }
+}
