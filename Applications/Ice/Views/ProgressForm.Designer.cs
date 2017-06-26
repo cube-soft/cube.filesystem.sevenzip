ef2c89a80b0b877566feb50233c82208ef44aadc
clown <tt.clown@gmail.com>
2017-06-26 17:42:09 +0900

    Add project for CubeICE

---
+++ Applications/Ice/Views/ProgressForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Cube.FileSystem.App.Ice
+{
+    partial class ProgressForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProgressForm));
+            this.RootPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.RemainLabel = new System.Windows.Forms.Label();
+            this.FileCountLabel = new System.Windows.Forms.Label();
+            this.MainProgressBar = new System.Windows.Forms.ProgressBar();
+            this.ElapseLabel = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.InterruptButton = new System.Windows.Forms.Button();
+            this.RootPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RootPanel
+            // 
+            this.RootPanel.ColumnCount = 2;
+            this.RootPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.RootPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.RootPanel.Controls.Add(this.ExitButton, 1, 4);
+            this.RootPanel.Controls.Add(this.RemainLabel, 1, 2);
+            this.RootPanel.Controls.Add(this.FileCountLabel, 0, 0);
+            this.RootPanel.Controls.Add(this.MainProgressBar, 0, 1);
+            this.RootPanel.Controls.Add(this.ElapseLabel, 0, 2);
+            this.RootPanel.Controls.Add(this.StatusLabel, 0, 3);
+            this.RootPanel.Controls.Add(this.InterruptButton, 0, 4);
+            this.RootPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RootPanel.Location = new System.Drawing.Point(0, 0);
+            this.RootPanel.Name = "RootPanel";
+            this.RootPanel.RowCount = 5;
+            this.RootPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.RootPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.RootPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.RootPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.RootPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.RootPanel.Size = new System.Drawing.Size(434, 181);
+            this.RootPanel.TabIndex = 0;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ExitButton.Location = new System.Drawing.Point(275, 141);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(100, 25);
+            this.ExitButton.TabIndex = 6;
+            this.ExitButton.Text = "キャンセル";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // RemainLabel
+            // 
+            this.RemainLabel.AutoSize = true;
+            this.RemainLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RemainLabel.Location = new System.Drawing.Point(229, 57);
+            this.RemainLabel.Margin = new System.Windows.Forms.Padding(12, 3, 12, 3);
+            this.RemainLabel.Name = "RemainLabel";
+            this.RemainLabel.Size = new System.Drawing.Size(193, 18);
+            this.RemainLabel.TabIndex = 3;
+            this.RemainLabel.Text = "残り時間 約 23:55:55";
+            this.RemainLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // FileCountLabel
+            // 
+            this.FileCountLabel.AutoSize = true;
+            this.RootPanel.SetColumnSpan(this.FileCountLabel, 2);
+            this.FileCountLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FileCountLabel.Location = new System.Drawing.Point(12, 3);
+            this.FileCountLabel.Margin = new System.Windows.Forms.Padding(12, 3, 12, 3);
+            this.FileCountLabel.Name = "FileCountLabel";
+            this.FileCountLabel.Size = new System.Drawing.Size(410, 24);
+            this.FileCountLabel.TabIndex = 0;
+            this.FileCountLabel.Text = "ファイル数 669 / 796";
+            this.FileCountLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // MainProgressBar
+            // 
+            this.RootPanel.SetColumnSpan(this.MainProgressBar, 2);
+            this.MainProgressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainProgressBar.Location = new System.Drawing.Point(14, 33);
+            this.MainProgressBar.Margin = new System.Windows.Forms.Padding(14, 3, 14, 3);
+            this.MainProgressBar.Name = "MainProgressBar";
+            this.MainProgressBar.Size = new System.Drawing.Size(406, 18);
+            this.MainProgressBar.Step = 1;
+            this.MainProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.MainProgressBar.TabIndex = 1;
+            // 
+            // ElapseLabel
+            // 
+            this.ElapseLabel.AutoSize = true;
+            this.ElapseLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ElapseLabel.Location = new System.Drawing.Point(12, 57);
+            this.ElapseLabel.Margin = new System.Windows.Forms.Padding(12, 3, 12, 3);
+            this.ElapseLabel.Name = "ElapseLabel";
+            this.ElapseLabel.Size = new System.Drawing.Size(193, 18);
+            this.ElapseLabel.TabIndex = 2;
+            this.ElapseLabel.Text = "経過時間 23:55:55";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoEllipsis = true;
+            this.StatusLabel.AutoSize = true;
+            this.RootPanel.SetColumnSpan(this.StatusLabel, 2);
+            this.StatusLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.StatusLabel.Location = new System.Drawing.Point(12, 84);
+            this.StatusLabel.Margin = new System.Windows.Forms.Padding(12, 6, 12, 6);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(410, 36);
+            this.StatusLabel.TabIndex = 4;
+            this.StatusLabel.Text = "ファイル圧縮・解凍処理の準備中です...";
+            // 
+            // InterruptButton
+            // 
+            this.InterruptButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.InterruptButton.Enabled = false;
+            this.InterruptButton.Location = new System.Drawing.Point(58, 141);
+            this.InterruptButton.Name = "InterruptButton";
+            this.InterruptButton.Size = new System.Drawing.Size(100, 25);
+            this.InterruptButton.TabIndex = 5;
+            this.InterruptButton.Text = "中断";
+            this.InterruptButton.UseVisualStyleBackColor = true;
+            // 
+            // ProgressForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(434, 181);
+            this.Controls.Add(this.RootPanel);
+            this.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ProgressForm";
+            this.Text = "CubeICE 解凍";
+            this.RootPanel.ResumeLayout(false);
+            this.RootPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel RootPanel;
+        private System.Windows.Forms.Label FileCountLabel;
+        private System.Windows.Forms.ProgressBar MainProgressBar;
+        private System.Windows.Forms.Label ElapseLabel;
+        private System.Windows.Forms.Label RemainLabel;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Button InterruptButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Applications/Ice/Views/ProgressForm.cs
@@ -0,0 +1,231 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// Copyright (c) 2010 CubeSoft, Inc.
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Lesser General Public License as
+/// published by the Free Software Foundation, either version 3 of the
+/// License, or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Lesser General Public License for more details.
+///
+/// You should have received a copy of the GNU Lesser General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Cube.FileSystem.App.Ice
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// ProgressForm
+    /// 
+    /// <summary>
+    /// 圧縮・展開の進捗状況を表示するクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public partial class ProgressForm : Form
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ProgressForm
+        ///
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public ProgressForm()
+        {
+            InitializeComponent();
+
+            Remain = TimeSpan.MinValue;
+            ExitButton.Click += (s, e) => Close();
+
+            _timer.Tick += (s, e) => UpdateElapseLabel();
+            _timer.Interval = 500;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Value
+        ///
+        /// <summary>
+        /// 進捗状況を示す値をパーセント単位で取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public int Value
+        {
+            get { return MainProgressBar.Value; }
+            set
+            {
+                if (MainProgressBar.Value == value) return;
+                MainProgressBar.Value = value;
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// FileIndex
+        ///
+        /// <summary>
+        /// 現在処理中のファイルのインデックスを取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public int FileIndex
+        {
+            get { return _fileIndex; }
+            set
+            {
+                if (_fileIndex == value) return;
+                _fileIndex = value;
+
+                UpdateFileCountLabel();
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// FileCount
+        ///
+        /// <summary>
+        /// 処理対象ファイルの合計値を取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public int FileCount
+        {
+            get { return _fileCount; }
+            set
+            {
+                if (_fileCount == value) return;
+                _fileCount = value;
+
+                UpdateFileCountLabel();
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Elapsed
+        ///
+        /// <summary>
+        /// 圧縮・展開処理開始からの経過時間を取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public TimeSpan Elapsed => _watch.Elapsed;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Remain
+        ///
+        /// <summary>
+        /// 圧縮・展開処理の残り時間を取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public TimeSpan Remain
+        {
+            get { return _remain; }
+            set
+            {
+                if (_remain == value) return;
+                _remain = value;
+
+                RemainLabel.Visible = value >= TimeSpan.Zero;
+                RemainLabel.Text = $"{Properties.Resources.MessageRemainTime} {GetTimeString(value)}";
+            }
+        }
+
+        #endregion
+
+        #region Implementations
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnShown
+        ///
+        /// <summary>
+        /// 初回表示時に実行されます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            UpdateFileCountLabel();
+            UpdateElapseLabel();
+
+            _timer.Start();
+            _watch.Start();
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// UpdateFileCountLabel
+        ///
+        /// <summary>
+        /// FileCountLabel の表示内容を更新します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void UpdateFileCountLabel()
+            => FileCountLabel.Text = $"{Properties.Resources.MessageFileCount} {FileIndex} / {FileCount}";
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// UpdateElapseLabel
+        ///
+        /// <summary>
+        /// ElapseLabel の表示内容を更新します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void UpdateElapseLabel()
+            => ElapseLabel.Text = $"{Properties.Resources.MessageElapsedTime} {GetTimeString(Elapsed)}";
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetTimeString
+        ///
+        /// <summary>
+        /// TimeSpan オブジェクトを文字列に変換します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private string GetTimeString(TimeSpan src)
+            => string.Format("{0:00}:{1:00}:{2:00}", src.TotalHours, src.Minutes, src.Seconds);
+
+        #region Fields
+        private int _fileIndex = 0;
+        private int _fileCount = 0;
+        private Stopwatch _watch = new Stopwatch();
+        private Timer _timer = new Timer();
+        private TimeSpan _remain = TimeSpan.Zero;
+        #endregion
+
+        #endregion
+    }
+}
