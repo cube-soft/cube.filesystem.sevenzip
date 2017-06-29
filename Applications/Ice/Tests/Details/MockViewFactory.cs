2cba9ad8784e5ff6a81a97ee604352870e957f09
clown <tt.clown@gmail.com>
2017-06-29 12:35:27 +0900

    Add file for MockViewFactory

---
+++ Applications/Ice/Tests/Details/MockViewFactory.cs
@@ -0,0 +1,40 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/* ------------------------------------------------------------------------- */
+namespace Cube.FileSystem.App.Ice.Tests
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// MockViewFactory
+    ///
+    /// <summary>
+    /// 各種ダミー View の生成用クラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    class MockViewFactory : ViewFactory
+    {
+        public override IProgressView CreateProgressView()
+            => new ProgressMockView();
+
+        public override void ShowPasswordView(QueryEventArgs<string, string> e)
+        {
+            e.Cancel = false;
+            e.Result = "password";
+        }
+    }
+}
