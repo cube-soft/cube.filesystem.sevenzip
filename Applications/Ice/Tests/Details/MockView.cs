857097c016f9891c2a3137ca7a6e0c12afdf3c8f
clown <tt.clown@gmail.com>
2017-06-29 12:35:49 +0900

    Implement MockView

--- Applications/Ice/Tests/Details/MockView.cs
+++ Applications/Ice/Tests/Details/MockView.cs
@@ -22,59 +22,410 @@ namespace Cube.FileSystem.App.Ice.Tests
 {
     /* --------------------------------------------------------------------- */
     ///
-    /// MockViewBase
+    /// MockView
     /// 
     /// <summary>
-    /// ダミー View の基底クラスです。
+    /// Cube.Forms.IForm を最小構成で実装した Mock クラスです。
     /// </summary>
     ///
     /* --------------------------------------------------------------------- */
-    class MockViewBase : Cube.Forms.IForm
+    class MockView : Cube.Forms.IForm
     {
         #region Properties
-        public bool Enabled { get; set; }
-        public bool Visible { get; set; }
-        public Point Location { get; set; }
-        public Size Size { get; set; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// EventAggregator
+        /// 
+        /// <summary>
+        /// イベントを集約するためのオブジェクトを取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
         public IEventAggregator EventAggregator { get; set; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Enabled
+        /// 
+        /// <summary>
+        /// コントロールが有効かどうかを示す値を取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public bool Enabled
+        {
+            get { return _enabled; }
+            set
+            {
+                if (_enabled == value) return;
+                _enabled = value;
+                OnEnabledChanged(EventArgs.Empty);
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Visible
+        /// 
+        /// <summary>
+        /// コントロールが表示されているかどうかを示す値を取得または
+        /// 設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public bool Visible
+        {
+            get { return _visible; }
+            set
+            {
+                if (_visible == value) return;
+                _visible = value;
+                OnVisibleChanged(EventArgs.Empty);
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Location
+        /// 
+        /// <summary>
+        /// 表示位置を取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Point Location
+        {
+            get { return _location; }
+            set
+            {
+                if (_location == value) return;
+                _location = value;
+                OnMove(EventArgs.Empty);
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Size
+        /// 
+        /// <summary>
+        /// 表示サイズを取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Size Size
+        {
+            get { return _size; }
+            set
+            {
+                if (_size == value) return;
+                _size = value;
+                OnResize(EventArgs.Empty);
+            }
+        }
+
         #endregion
 
         #region Events
+
+        #region Load
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Load
+        /// 
+        /// <summary>
+        /// オブジェクトがロードされた時に発生するイベントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
         public event EventHandler Load;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnLoad
+        /// 
+        /// <summary>
+        /// Load イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void OnLoad(EventArgs e)
+            => Load?.Invoke(this, e);
+
+        #endregion
+
+        #region Activated
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Activated
+        /// 
+        /// <summary>
+        /// View がアクティブ化した時に発生するイベントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
         public event EventHandler Activated;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnActivated
+        /// 
+        /// <summary>
+        /// Activated イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void OnActivated(EventArgs e)
+            => Activated?.Invoke(this, e);
+
+        #endregion
+
+        #region Deactivate
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Deactivate
+        /// 
+        /// <summary>
+        /// View が非アクティブ化した時に発生するイベントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
         public event EventHandler Deactivate;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnDeactivate
+        /// 
+        /// <summary>
+        /// Deactivate イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void OnDeactivate(EventArgs e)
+            => Deactivate?.Invoke(this, e);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// RaiseDeactivate
+        /// 
+        /// <summary>
+        /// Deactivate イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public virtual void RaiseDeactivate()
+            => OnDeactivate(EventArgs.Empty);
+
+        #endregion
+
+        #region Click
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Click
+        /// 
+        /// <summary>
+        /// クリック時に発生するイベントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
         public event EventHandler Click;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnClick
+        /// 
+        /// <summary>
+        /// Click イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void OnClick(EventArgs e)
+            => Click?.Invoke(this, e);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// RaiseClick
+        /// 
+        /// <summary>
+        /// Click イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public virtual void RaiseClick()
+            => OnClick(EventArgs.Empty);
+
+        #endregion
+
+        #region EnabledChanged
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// EnabledChanged
+        /// 
+        /// <summary>
+        /// Enabled が変更された時に発生するイベントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
         public event EventHandler EnabledChanged;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnEnabledChanged
+        /// 
+        /// <summary>
+        /// EnabledChanged イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void OnEnabledChanged(EventArgs e)
+            => EnabledChanged?.Invoke(this, e);
+
+        #endregion
+
+        #region VisibleChanged
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// VisibleChanged
+        /// 
+        /// <summary>
+        /// Visible が変更された時に発生するイベントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
         public event EventHandler VisibleChanged;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnVisibleChanged
+        /// 
+        /// <summary>
+        /// VisibleChanged イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void OnVisibleChanged(EventArgs e)
+            => VisibleChanged?.Invoke(this, e);
+
+        #endregion
+
+        #region Move
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Move
+        /// 
+        /// <summary>
+        /// 移動時に発生するイベントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
         public event EventHandler Move;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnMove
+        /// 
+        /// <summary>
+        /// Move イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void OnMove(EventArgs e)
+            => Move?.Invoke(this, e);
+
+        #endregion
+
+        #region Resize
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Resize
+        /// 
+        /// <summary>
+        /// リサイズ時に発生するイベントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
         public event EventHandler Resize;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnResize
+        /// 
+        /// <summary>
+        /// Move イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void OnResize(EventArgs e)
+            => Resize?.Invoke(this, e);
+
+        #endregion
+
         #endregion
 
         #region Methods
-        public void Activate() { }
-        public void Close() { }
-        public void Show() { }
-        #endregion
-    }
 
-    /* --------------------------------------------------------------------- */
-    ///
-    /// MockViewFactory
-    ///
-    /// <summary>
-    /// 各種ダミー View の生成用クラスです。
-    /// </summary>
-    ///
-    /* --------------------------------------------------------------------- */
-    class MockViewFactory : ViewFactory
-    {
-        public override IProgressView CreateProgressView()
-            => new ProgressMockView();
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Activate
+        /// 
+        /// <summary>
+        /// View をアクティブ化します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public virtual void Activate() => OnActivated(EventArgs.Empty);
 
-        public override void ShowPasswordView(QueryEventArgs<string, string> e)
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Close
+        /// 
+        /// <summary>
+        /// View を閉じます。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// MockView では、Visible プロパティを false に設定します。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        public virtual void Close() => Visible = false;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Show
+        /// 
+        /// <summary>
+        /// View を表示します。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// MockView では Load イベントを発生させた後、Visible プロパティを
+        /// true に設定します。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        public virtual void Show()
         {
-            e.Cancel = false;
-            e.Result = "password";
+            OnLoad(EventArgs.Empty);
+            Visible = true;
         }
+
+        #endregion
+
+        #region Fields
+        private bool _enabled = true;
+        private bool _visible = false;
+        private Point _location;
+        private Size _size;
+        #endregion
     }
 }
