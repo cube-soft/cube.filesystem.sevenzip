84c524ca28fd07e3928bd42644bd942443845e7e
clown <tt.clown@gmail.com>
2017-07-02 23:08:55 +0900

    Add skeleton for ArchivePresenter

---
+++ Applications/Ice/Presenters/ArchiveFacade.cs
@@ -0,0 +1,65 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+
+namespace Cube.FileSystem.App.Ice
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// ArchiveFacade
+    ///
+    /// <summary>
+    /// 圧縮処理を実行するクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class ArchiveFacade
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ArchiveFacade
+        /// 
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        /// 
+        /// <param name="args">コマンドライン</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public ArchiveFacade(Arguments args) { }
+
+        #endregion
+
+        #region Methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Start
+        /// 
+        /// <summary>
+        /// 圧縮を開始します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void Start() { }
+
+        #endregion
+    }
+}
---
+++ Applications/Ice/Presenters/ArchivePresenter.cs
@@ -0,0 +1,81 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/* ------------------------------------------------------------------------- */
+using Cube.Tasks;
+
+namespace Cube.FileSystem.App.Ice
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// ArchivePresenter
+    ///
+    /// <summary>
+    /// 圧縮用の Presenter クラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class ArchivePresenter
+        : Cube.Forms.PresenterBase<IProgressView, ArchiveFacade, SettingsFolder>
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ArchivePresenter
+        /// 
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        /// 
+        /// <param name="view">View オブジェクト</param>
+        /// <param name="args">コマンドライン</param>
+        /// <param name="settings">ユーザ設定</param>
+        /// <param name="events">イベント集約用オブジェクト</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public ArchivePresenter(IProgressView view, Arguments args,
+            SettingsFolder settings, IEventAggregator events)
+            : base(view, new ArchiveFacade(args), settings, events)
+        {
+            // View
+            View.EventAggregator = EventAggregator;
+
+            // EventAggregator
+            EventAggregator.GetEvents()?.Show.Subscribe(WhenShow);
+        }
+
+        #endregion
+
+        #region Handlers
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// WhenShow
+        /// 
+        /// <summary>
+        /// 画面表示時に実行されるハンドラです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void WhenShow() => Async(() =>
+        {
+
+        }).Forget();
+
+        #endregion
+    }
+}
