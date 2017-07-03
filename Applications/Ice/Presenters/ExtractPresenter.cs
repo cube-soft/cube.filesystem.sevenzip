1bc5bde0c0baacc3499f0f6bb83f0c44190ffc37
clown <tt.clown@gmail.com>
2017-07-03 19:18:38 +0900

    Add for setting icon

--- Applications/Ice/Presenters/ExtractPresenter.cs
+++ Applications/Ice/Presenters/ExtractPresenter.cs
@@ -81,6 +81,7 @@ namespace Cube.FileSystem.App.Ice
         {
             Sync(() =>
             {
+                View.Icon = Properties.Resources.Extract;
                 View.Status = Properties.Resources.MessagePreExtract;
                 View.Start();
             });
