50ae43a485b94b6cca1c9bafab25f8a579ae850f
clown <tt.clown@gmail.com>
2017-07-22 12:29:49 +0900

    Update tests

--- Tests/SevenZip/ArchiveWriterTest.cs
+++ Tests/SevenZip/ArchiveWriterTest.cs
@@ -47,8 +47,8 @@ namespace Cube.FileSystem.Tests
         /// 
         /* ----------------------------------------------------------------- */
         [TestCaseSource(nameof(Archive_TestCases))]
-        public void Archive(Format format, string filename, string password,
-            string[] items, ArchiveOption option, long size)
+        public long Archive(Format format, string filename, string password,
+            string[] items, ArchiveOption option)
         {
             var dest = Result(filename);
 
@@ -59,10 +59,12 @@ namespace Cube.FileSystem.Tests
                 writer.Save(dest, password);
             }
 
-            var info = new System.IO.FileInfo(dest);
-            Assert.That(info.Exists, Is.True);
-            Assert.That(info.Length, Is.EqualTo(size));
+            return new System.IO.FileInfo(dest).Length;
         }
+
+        #endregion
+
+        #region TestCases
 
         /* ----------------------------------------------------------------- */
         ///
@@ -81,41 +83,36 @@ namespace Cube.FileSystem.Tests
                     "ZipSingle.zip",
                     "",
                     new[] { "Sample.txt" },
-                    null,
-                    167L
-                );
+                    null
+                ).Returns(167L);
 
                 yield return new TestCaseData(Format.Zip,
                     "ZipDirectory.zip",
                     "",
                     new[] { "Archive" },
-                    null,
-                    11873L
-                );
+                    null
+                ).Returns(11873L);
 
                 yield return new TestCaseData(Format.Zip,
                     "ZipUltra.zip",
                     "",
                     new[] { "Sample.txt", "Archive" },
-                    new ZipOption { CompressionLevel = CompressionLevel.Ultra },
-                    11822L
-                );
+                    new ZipOption { CompressionLevel = CompressionLevel.Ultra }
+                ).Returns(11822L);
 
                 yield return new TestCaseData(Format.Zip,
                     "ZipLzma.zip",
                     "",
                     new[] { "Sample.txt", "Archive" },
-                    new ZipOption { CompressionMethod = CompressionMethod.Lzma },
-                    11645L
-                );
+                    new ZipOption { CompressionMethod = CompressionMethod.Lzma }
+                ).Returns(11645L);
 
                 yield return new TestCaseData(Format.Zip,
                     "ZipPassword.zip",
                     "password",
                     new[] { "Sample.txt" },
-                    null,
-                    179L
-                );
+                    null
+                ).Returns(179L);
 
                 yield return new TestCaseData(Format.Zip,
                     "ZipAes256.zip",
@@ -125,33 +122,29 @@ namespace Cube.FileSystem.Tests
                     {
                         EncryptionMethod = EncryptionMethod.Aes256,
                         IsEncrypted = true,
-                    },
-                    217L
-                );
+                    }
+                ).Returns(217L);
 
                 yield return new TestCaseData(Format.Tar,
                     "TarTest.tar",
                     "",
                     new[] { "Sample.txt", "Archive" },
-                    null,
-                    38400L
-                );
+                    null
+                ).Returns(38400L);
 
                 yield return new TestCaseData(Format.BZip2,
                     "BZip2Test.bz",
                     "",
                     new[] { "Sample.txt" },
-                    null,
-                    51L
-                );
+                    null
+                ).Returns(51L);
 
                 yield return new TestCaseData(Format.GZip,
                     "GZipTest.gz",
                     "",
                     new[] { "Sample.txt" },
-                    null,
-                    47L
-                );
+                    null
+                ).Returns(47L);
             }
         }
 
