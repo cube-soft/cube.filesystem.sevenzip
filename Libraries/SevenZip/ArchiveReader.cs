2b0f9a1c031570b1d26106f4bc76116af492a725
clown <tt.clown@gmail.com>
2017-07-25 23:17:03 +0900

    Fix for using FileSystem.Operator

--- Libraries/SevenZip/ArchiveReader.cs
+++ Libraries/SevenZip/ArchiveReader.cs
@@ -228,7 +228,7 @@ namespace Cube.FileSystem.SevenZip
         /* ----------------------------------------------------------------- */
         private void Open(IQuery<string, string> password)
         {
-            Format = Formats.FromFile(Source);
+            Format = Formats.FromFile(Source, _io);
             if (Format == Format.Unknown) throw new NotSupportedException();
 
             _stream = new ArchiveStreamReader(_io.OpenRead(Source));
