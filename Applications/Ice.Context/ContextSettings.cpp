781365e0712e3ea59c93ea0695e3f1d366e34ddf
clown <tt.clown@gmail.com>
2017-07-31 19:20:33 +0900

    Add ContextSettings.

---
+++ Applications/Ice.Context/ContextSettings.cpp
@@ -0,0 +1,49 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/* ------------------------------------------------------------------------- */
+#include "ContextSettings.h"
+
+namespace Cube {
+namespace FileSystem {
+namespace Ice {
+
+/* ------------------------------------------------------------------------- */
+///
+/// ContextSettings
+/// 
+/// <summary>
+/// オブジェクトを初期化します。
+/// </summary>
+///
+/* ------------------------------------------------------------------------- */
+ContextSettings::ContextSettings() :
+    preset_(0x00007ff3) {}
+
+/* ------------------------------------------------------------------------- */
+///
+/// Load
+/// 
+/// <summary>
+/// ユーザ設定を読み込みます。
+/// </summary>
+///
+/* ------------------------------------------------------------------------- */
+void ContextSettings::Load() {
+    // TODO: implementations
+}
+
+}}} // Cube::FileSystem::Ice
