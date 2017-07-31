97aa7c49a648ce1d1a07d73fedc6c2465c71c6dd
clown <tt.clown@gmail.com>
2017-07-31 19:13:06 +0900

    Refactoring.

--- Applications/Ice.Context/ContextMenuItem.cpp
+++ Applications/Ice.Context/ContextMenuItem.cpp
@@ -16,7 +16,6 @@
 ///
 /* ------------------------------------------------------------------------- */
 #include "ContextMenuItem.h"
-#include "Resources.h"
 
 namespace Cube {
 namespace FileSystem {
@@ -30,7 +29,6 @@ namespace Ice {
 /// オブジェクトを初期化します。
 /// </summary>
 ///
-/// <param name="index">表示順序を示すインデックス</param>
 /// <param name="name">表示名</param>
 /// <param name="filename">クリック時に実行されるプログラム</param>
 /// <param name="arguments">コマンドライン引数</param>
@@ -38,13 +36,12 @@ namespace Ice {
 ///
 /* ------------------------------------------------------------------------- */
 ContextMenuItem::ContextMenuItem(
-    int index,
     const ContextMenuItem::TString& name,
     const ContextMenuItem::TString& filename,
     const ContextMenuItem::TString& arguments,
     const ContextMenuItem::TString& icon
 ) :
-    index_(index), name_(name), filename_(filename),
+    index_(-1), name_(name), filename_(filename),
     arguments_(arguments), icon_(icon), children_() {}
 
 /* ------------------------------------------------------------------------- */
@@ -66,11 +63,10 @@ ContextMenuItem::ContextMenuItem(
 ///
 /* ------------------------------------------------------------------------- */
 ContextMenuItem::ContextMenuItem(
-    int index,
     const ContextMenuItem::TString& name,
     const ContextMenuItem::TString& icon
 ) :
-    index_(index), name_(name), filename_(),
+    index_(-1), name_(name), filename_(),
     arguments_(), icon_(icon), children_() {}
 
 /* ------------------------------------------------------------------------- */
@@ -87,71 +83,5 @@ ContextMenuItem::ContextMenuItem(
 ContextMenuItem::ContextMenuItem(const ContextMenuItem& cp) :
     index_(cp.index_), name_(cp.name_), filename_(cp.filename_),
     arguments_(cp.arguments_), icon_(cp.icon_), children_(cp.children_) {}
-
-/* ------------------------------------------------------------------------- */
-///
-/// GetMenuItem
-/// 
-/// <summary>
-/// PresetMenu に対応する ContextMenuItem オブジェクトを取得します。
-/// </summary>
-///
-/// <param name="preset">PresetMenu ID</param>
-/// <param name="index">表示インデックス</param>
-/// <param name="exe">実行プログラム</param>
-///
-/// <returns>コンテキストメニュー項目</returns>
-///
-/* ------------------------------------------------------------------------- */
-ContextMenuItem GetContextMenuItem(int preset, int index, const std::basic_string<TCHAR>& exe) {
-    switch (preset)
-    {
-    case PresetMenu::Archive:
-        return ContextMenuItem(index, CUBEICE_MENU_ARCHIVE,  exe + _T(",1")); // Folder
-    case PresetMenu::Extract:
-        return ContextMenuItem(index, CUBEICE_MENU_EXTRACT,  exe + _T(",2")); // Folder
-    case PresetMenu::Mail:
-        return ContextMenuItem(index, CUBEICE_MENU_MAIL,     exe + _T(",1")); // Folder
-    case PresetMenu::ArchiveZip:
-        return ContextMenuItem(index, CUBEICE_MENU_ZIP,      exe, _T("/c:zip"), _T(""));
-    case PresetMenu::ArchiveZipPassword:
-        return ContextMenuItem(index, CUBEICE_MENU_PASSWORD, exe, _T("/c:zip /p"), _T(""));
-    case PresetMenu::ArchiveSevenZip:
-        return ContextMenuItem(index, CUBEICE_MENU_7Z,       exe, _T("/c:7z"), _T(""));
-    case PresetMenu::ArchiveBZip2:
-        return ContextMenuItem(index, CUBEICE_MENU_BZIP2,    exe, _T("/c:bzip2"), _T(""));
-    case PresetMenu::ArchiveGZip:
-        return ContextMenuItem(index, CUBEICE_MENU_GZIP,     exe, _T("/c:gzip"), _T(""));
-    case PresetMenu::ArchiveSfx:
-        return ContextMenuItem(index, CUBEICE_MENU_SFX,      exe, _T("/c:exe"), _T(""));
-    case PresetMenu::ArchiveDetail:
-        return ContextMenuItem(index, CUBEICE_MENU_DETAIL,   exe, _T("/c:detail"), _T(""));
-    case PresetMenu::ExtractDesktop:
-        return ContextMenuItem(index, CUBEICE_MENU_EDESKTOP, exe, _T("/x /o:desktop"), _T(""));
-    case PresetMenu::ExtractMyDocuments:
-        return ContextMenuItem(index, CUBEICE_MENU_EMYDOCS,  exe, _T("/x /o:mydocuments"), _T(""));
-    case PresetMenu::ExtractRuntime:
-        return ContextMenuItem(index, CUBEICE_MENU_ERUNTIME, exe, _T("/x /o:runtime"), _T(""));
-    case PresetMenu::ExtractSource:
-        return ContextMenuItem(index, CUBEICE_MENU_ESOURCE,  exe, _T("/x /o:source"), _T(""));
-    case PresetMenu::MailBZip2:
-        return ContextMenuItem(index, CUBEICE_MENU_BZIP2,    exe, _T("/c:bzip2 /m"), _T(""));
-    case PresetMenu::MailDetail:
-        return ContextMenuItem(index, CUBEICE_MENU_DETAIL,   exe, _T("/c:detail /m"), _T(""));
-    case PresetMenu::MailGZip:
-        return ContextMenuItem(index, CUBEICE_MENU_GZIP,     exe, _T("/c:gzip /m"), _T(""));
-    case PresetMenu::MailSevenZip:
-        return ContextMenuItem(index, CUBEICE_MENU_7Z,       exe, _T("/c:7z /m"), _T(""));
-    case PresetMenu::MailSfx:
-        return ContextMenuItem(index, CUBEICE_MENU_SFX,      exe, _T("/c:exe /m"), _T(""));
-    case PresetMenu::MailZip:
-        return ContextMenuItem(index, CUBEICE_MENU_ZIP,      exe, _T("/c:zip /m"), _T(""));
-    case PresetMenu::MailZipPassword:
-        return ContextMenuItem(index, CUBEICE_MENU_PASSWORD, exe, _T("/c:zip /p /m"), _T(""));
-    default:
-        break;
-    }
-    return ContextMenuItem(index, _T("CubeICE"), exe, _T(""), exe + _T(",0"));
-}
 
 }}} // Cube::FileSystem::Ice
--- Applications/Ice.Context/ContextMenuItem.h
+++ Applications/Ice.Context/ContextMenuItem.h
@@ -20,114 +20,59 @@
 #include <tchar.h>
 #include <string>
 #include <vector>
-#include <map>
 
 namespace Cube {
 namespace FileSystem {
 namespace Ice {
-    /* --------------------------------------------------------------------- */
-    ///
-    /// ContextMenuItem
-    /// 
-    /// <summary>
-    /// コンテキストメニューの一項目を表すクラスです。
-    /// </summary>
-    ///
-    /* --------------------------------------------------------------------- */
-    class ContextMenuItem {
-    public:
-        typedef std::basic_string<TCHAR> TString;
-        typedef std::vector<ContextMenuItem> ContextMenuVector;
 
-        ContextMenuItem() = delete;
-        ContextMenuItem(int, const TString&, const TString&, const TString&, const TString&);
-        ContextMenuItem(int, const TString&, const TString&);
-        ContextMenuItem(const ContextMenuItem&);
-        virtual ~ContextMenuItem() noexcept {}
+/* ------------------------------------------------------------------------- */
+///
+/// ContextMenuItem
+/// 
+/// <summary>
+/// コンテキストメニューの一項目を表すクラスです。
+/// </summary>
+///
+/* ------------------------------------------------------------------------- */
+class ContextMenuItem {
+public:
+    typedef std::basic_string<TCHAR> TString;
+    typedef std::vector<ContextMenuItem> ContextMenuVector;
 
-        const int& Index() const { return index_; }
-        const TString& DisplayName() const { return name_; }
-        const TString& FileName() const { return filename_; }
-        const TString& Arguments() const { return arguments_; }
-        const TString& IconLocation() const { return icon_; }
-        const ContextMenuVector& Children() const { return children_; }
+    ContextMenuItem() = delete;
+    ContextMenuItem(const TString&, const TString&, const TString&, const TString&);
+    ContextMenuItem(const TString&, const TString&);
+    ContextMenuItem(const ContextMenuItem&);
+    virtual ~ContextMenuItem() noexcept {}
 
-        int& Index() { return index_; }
-        TString& DisplayName() { return name_; }
-        TString& FileName() { return filename_; }
-        TString& Arguments() { return arguments_; }
-        TString& IconLocation() { return icon_; }
-        ContextMenuVector& Children() { return children_; }
+    const int& Index() const { return index_; }
+    const TString& DisplayName() const { return name_; }
+    const TString& FileName() const { return filename_; }
+    const TString& Arguments() const { return arguments_; }
+    const TString& IconLocation() const { return icon_; }
+    const ContextMenuVector& Children() const { return children_; }
 
-        friend bool operator==(const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ == rhs.index_; }
-        friend bool operator!=(const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ != rhs.index_; }
-        friend bool operator< (const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ <  rhs.index_; }
-        friend bool operator<=(const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ <= rhs.index_; }
-        friend bool operator> (const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ >  rhs.index_; }
-        friend bool operator>=(const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ >= rhs.index_; }
+    int& Index() { return index_; }
+    TString& DisplayName() { return name_; }
+    TString& FileName() { return filename_; }
+    TString& Arguments() { return arguments_; }
+    TString& IconLocation() { return icon_; }
+    ContextMenuVector& Children() { return children_; }
 
-    private:
-        int index_;
-        TString name_;
-        TString filename_;
-        TString arguments_;
-        TString icon_;
-        ContextMenuVector children_;
-    };
+    friend bool operator==(const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ == rhs.index_; }
+    friend bool operator!=(const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ != rhs.index_; }
+    friend bool operator< (const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ <  rhs.index_; }
+    friend bool operator<=(const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ <= rhs.index_; }
+    friend bool operator> (const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ >  rhs.index_; }
+    friend bool operator>=(const ContextMenuItem& lhs, const ContextMenuItem& rhs) { return lhs.index_ >= rhs.index_; }
 
-    /* --------------------------------------------------------------------- */
-    ///
-    /// PresetMenu
-    /// 
-    /// <summary>
-    /// 予め定義されたメニューを表す列挙型です。
-    /// </summary>
-    ///
-    /* --------------------------------------------------------------------- */
-    namespace PresetMenu {
-        enum {
-            Archive             = 0x00000001,
-            Extract             = 0x00000002,
-            Settings            = 0x00000004,
-            Mail                = 0x00000008,
+private:
+    int index_;
+    TString name_;
+    TString filename_;
+    TString arguments_;
+    TString icon_;
+    ContextMenuVector children_;
+};
 
-            ExtractSource       = 0x00000010,
-            ExtractDesktop      = 0x00000020,
-            ExtractRuntime      = 0x00000040,
-            ExtractMyDocuments  = 0x00000080,
-
-            ArchiveZip          = 0x00000100,
-            ArchiveZipPassword  = 0x00000200,
-            ArchiveSevenZip     = 0x00000400,
-            ArchiveBZip2        = 0x00000800,
-            ArchiveGZip         = 0x00001000,
-            ArchiveDetail       = 0x00002000,
-            ArchiveSfx          = 0x00004000,
-
-            MailZip             = 0x00010000,
-            MailZipPassword     = 0x00020000,
-            MailSevenZip        = 0x00040000,
-            MailBZip2           = 0x00080000,
-            MailGZip            = 0x00100000,
-            MailDetail          = 0x00200000,
-            MailSfx             = 0x00400000,
-        };
-    }
-
-    /* --------------------------------------------------------------------- */
-    ///
-    /// GetContextMenuItem
-    /// 
-    /// <summary>
-    /// PresetMenu に対応する ContextMenuItem オブジェクトを取得します。
-    /// </summary>
-    ///
-    /// <param name="preset">PresetMenu ID</param>
-    /// <param name="index">表示インデックス</param>
-    /// <param name="exe">実行プログラム</param>
-    ///
-    /// <returns>コンテキストメニュー項目</returns>
-    ///
-    /* --------------------------------------------------------------------- */
-    ContextMenuItem GetContextMenuItem(int preset, int index, const std::basic_string<TCHAR>& exe);
 }}}
