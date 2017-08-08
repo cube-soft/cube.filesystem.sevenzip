2bdb9b53f61d2d5c0b8f2a45d32035fda3ba84ad
clown <tt.clown@gmail.com>
2017-08-08 09:30:39 +0900

    Remove DeleteSource method.

--- Applications/Ice/Presenters/ExtractFacade.cs
+++ Applications/Ice/Presenters/ExtractFacade.cs
@@ -111,7 +111,6 @@ namespace Cube.FileSystem.App.Ice
                     SetTmp(Destination);
                     PreExtract(reader);
                     Extract(reader);
-                    DeleteSource();
                     Open(IO.Combine(Destination, OpenDirectoryName), Settings.Value.Extract.OpenDirectory);
                 }
                 catch (UserCancelException /* err */) { /* user cancel */ }
@@ -293,21 +292,6 @@ namespace Cube.FileSystem.App.Ice
                 Overwrite(src, dest);
             }
             else Move(src, dest);
-        }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// DeleteSource
-        /// 
-        /// <summary>
-        /// 圧縮ファイルを削除します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        private void DeleteSource()
-        {
-            if (!Settings.Value.Extract.DeleteSource) return;
-            IO.Delete(Source);
         }
 
         /* ----------------------------------------------------------------- */
