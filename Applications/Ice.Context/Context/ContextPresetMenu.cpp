63bc3da1759662208b7b198090f9eb032ec706f8
clown <tt.clown@gmail.com>
2017-08-10 13:00:25 +0900

    Fix errors.

--- Applications/Ice.Context/Context/ContextPresetMenu.cpp
+++ Applications/Ice.Context/Context/ContextPresetMenu.cpp
@@ -41,47 +41,47 @@ static ContextMenuItem GetContextMenuItem(int id, const std::basic_string<TCHAR>
     switch (id)
     {
     case PresetMenu::Archive:
-        return ContextMenuItem(CUBEICE_MENU_ARCHIVE, exe + _T(",1")); // Folder
+        return ContextMenuItem(CUBEICE_MENU_ARCHIVE,  exe + _T(",1")); // Folder
     case PresetMenu::Extract:
-        return ContextMenuItem(CUBEICE_MENU_EXTRACT, exe + _T(",2")); // Folder
+        return ContextMenuItem(CUBEICE_MENU_EXTRACT,  exe + _T(",2")); // Folder
     case PresetMenu::Mail:
-        return ContextMenuItem(CUBEICE_MENU_MAIL, exe + _T(",1")); // Folder
+        return ContextMenuItem(CUBEICE_MENU_MAIL,     exe + _T(",1")); // Folder
     case PresetMenu::ArchiveZip:
-        return ContextMenuItem(CUBEICE_MENU_ZIP, exe, _T("/c:zip"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_ZIP,      exe, _T("/c:zip"),            _T(""));
     case PresetMenu::ArchiveZipPassword:
-        return ContextMenuItem(CUBEICE_MENU_PASSWORD, exe, _T("/c:zip /p"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_PASSWORD, exe, _T("/c:zip /p"),         _T(""));
     case PresetMenu::ArchiveSevenZip:
-        return ContextMenuItem(CUBEICE_MENU_7Z, exe, _T("/c:7z"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_7Z,       exe, _T("/c:7z"),             _T(""));
     case PresetMenu::ArchiveBZip2:
-        return ContextMenuItem(CUBEICE_MENU_BZIP2, exe, _T("/c:bzip2"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_BZIP2,    exe, _T("/c:bzip2"),          _T(""));
     case PresetMenu::ArchiveGZip:
-        return ContextMenuItem(CUBEICE_MENU_GZIP, exe, _T("/c:gzip"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_GZIP,     exe, _T("/c:gzip"),           _T(""));
     case PresetMenu::ArchiveSfx:
-        return ContextMenuItem(CUBEICE_MENU_SFX, exe, _T("/c:exe"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_SFX,      exe, _T("/c:exe"),            _T(""));
     case PresetMenu::ArchiveDetail:
-        return ContextMenuItem(CUBEICE_MENU_DETAIL, exe, _T("/c:detail"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_DETAIL,   exe, _T("/c:detail"),         _T(""));
     case PresetMenu::ExtractDesktop:
-        return ContextMenuItem(CUBEICE_MENU_EDESKTOP, exe, _T("/x /o:desktop"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_EDESKTOP, exe, _T("/x /o:desktop"),     _T(""));
     case PresetMenu::ExtractMyDocuments:
-        return ContextMenuItem(CUBEICE_MENU_EMYDOCS, exe, _T("/x /o:mydocuments"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_EMYDOCS,  exe, _T("/x /o:mydocuments"), _T(""));
     case PresetMenu::ExtractRuntime:
-        return ContextMenuItem(CUBEICE_MENU_ERUNTIME, exe, _T("/x /o:runtime"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_ERUNTIME, exe, _T("/x /o:runtime"),     _T(""));
     case PresetMenu::ExtractSource:
-        return ContextMenuItem(CUBEICE_MENU_ESOURCE, exe, _T("/x /o:source"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_ESOURCE,  exe, _T("/x /o:source"),      _T(""));
     case PresetMenu::MailBZip2:
-        return ContextMenuItem(CUBEICE_MENU_BZIP2, exe, _T("/c:bzip2 /m"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_BZIP2,    exe, _T("/c:bzip2 /m"),       _T(""));
     case PresetMenu::MailDetail:
-        return ContextMenuItem(CUBEICE_MENU_DETAIL, exe, _T("/c:detail /m"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_DETAIL,   exe, _T("/c:detail /m"),      _T(""));
     case PresetMenu::MailGZip:
-        return ContextMenuItem(CUBEICE_MENU_GZIP, exe, _T("/c:gzip /m"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_GZIP,     exe, _T("/c:gzip /m"),        _T(""));
     case PresetMenu::MailSevenZip:
-        return ContextMenuItem(CUBEICE_MENU_7Z, exe, _T("/c:7z /m"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_7Z,       exe, _T("/c:7z /m"),          _T(""));
     case PresetMenu::MailSfx:
-        return ContextMenuItem(CUBEICE_MENU_SFX, exe, _T("/c:exe /m"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_SFX,      exe, _T("/c:exe /m"),         _T(""));
     case PresetMenu::MailZip:
-        return ContextMenuItem(CUBEICE_MENU_ZIP, exe, _T("/c:zip /m"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_ZIP,      exe, _T("/c:zip /m"),         _T(""));
     case PresetMenu::MailZipPassword:
-        return ContextMenuItem(CUBEICE_MENU_PASSWORD, exe, _T("/c:zip /p /m"), _T(""));
+        return ContextMenuItem(CUBEICE_MENU_PASSWORD, exe, _T("/c:zip /p /m"),      _T(""));
     default:
         break;
     }
@@ -124,34 +124,40 @@ std::vector<ContextMenuItem> GetContextMenuItems(int menu, const std::basic_stri
 
     if ((menu & PresetMenu::Archive) != 0) {
         auto archive = GetContextMenuItem(PresetMenu::Archive, exe);
-        AddContextMenuItem(archive, menu, PresetMenu::ArchiveZip, exe);
+
+        AddContextMenuItem(archive, menu, PresetMenu::ArchiveZip,         exe);
         AddContextMenuItem(archive, menu, PresetMenu::ArchiveZipPassword, exe);
-        AddContextMenuItem(archive, menu, PresetMenu::ArchiveSevenZip, exe);
-        AddContextMenuItem(archive, menu, PresetMenu::ArchiveBZip2, exe);
-        AddContextMenuItem(archive, menu, PresetMenu::ArchiveGZip, exe);
-        AddContextMenuItem(archive, menu, PresetMenu::ArchiveSfx, exe);
-        AddContextMenuItem(archive, menu, PresetMenu::ArchiveDetail, exe);
+        AddContextMenuItem(archive, menu, PresetMenu::ArchiveSevenZip,    exe);
+        AddContextMenuItem(archive, menu, PresetMenu::ArchiveBZip2,       exe);
+        AddContextMenuItem(archive, menu, PresetMenu::ArchiveGZip,        exe);
+        AddContextMenuItem(archive, menu, PresetMenu::ArchiveSfx,         exe);
+        AddContextMenuItem(archive, menu, PresetMenu::ArchiveDetail,      exe);
+
         if (archive.Children().size() > 0) dest.push_back(archive);
     }
 
     if ((menu & PresetMenu::Extract) != 0) {
         auto extract = GetContextMenuItem(PresetMenu::Extract, exe);
-        AddContextMenuItem(extract, menu, PresetMenu::ExtractSource, exe);
-        AddContextMenuItem(extract, menu, PresetMenu::ExtractDesktop, exe);
+
+        AddContextMenuItem(extract, menu, PresetMenu::ExtractSource,      exe);
+        AddContextMenuItem(extract, menu, PresetMenu::ExtractDesktop,     exe);
         AddContextMenuItem(extract, menu, PresetMenu::ExtractMyDocuments, exe);
-        AddContextMenuItem(extract, menu, PresetMenu::ExtractRuntime, exe);
+        AddContextMenuItem(extract, menu, PresetMenu::ExtractRuntime,     exe);
+
         if (extract.Children().size() > 0) dest.push_back(extract);
     }
 
     if ((menu & PresetMenu::Mail) != 0) {
         auto mail = GetContextMenuItem(PresetMenu::Mail, exe);
-        AddContextMenuItem(mail, menu, PresetMenu::ArchiveZip, exe);
-        AddContextMenuItem(mail, menu, PresetMenu::ArchiveZipPassword, exe);
-        AddContextMenuItem(mail, menu, PresetMenu::ArchiveSevenZip, exe);
-        AddContextMenuItem(mail, menu, PresetMenu::ArchiveBZip2, exe);
-        AddContextMenuItem(mail, menu, PresetMenu::ArchiveGZip, exe);
-        AddContextMenuItem(mail, menu, PresetMenu::ArchiveSfx, exe);
-        AddContextMenuItem(mail, menu, PresetMenu::ArchiveDetail, exe);
+
+        AddContextMenuItem(mail, menu, PresetMenu::MailZip,         exe);
+        AddContextMenuItem(mail, menu, PresetMenu::MailZipPassword, exe);
+        AddContextMenuItem(mail, menu, PresetMenu::MailSevenZip,    exe);
+        AddContextMenuItem(mail, menu, PresetMenu::MailBZip2,       exe);
+        AddContextMenuItem(mail, menu, PresetMenu::MailGZip,        exe);
+        AddContextMenuItem(mail, menu, PresetMenu::MailSfx,         exe);
+        AddContextMenuItem(mail, menu, PresetMenu::MailDetail,      exe);
+
         if (mail.Children().size() > 0) dest.push_back(mail);
     }
 
