1c930b5cf1a78dcec0606c7f5d8e11ffa57e9e05
clown <tt.clown@gmail.com>
2017-08-18 16:23:46 +0900

    Remove assert.

--- Tests/SevenZip/ArchiveReaderTest.cs
+++ Tests/SevenZip/ArchiveReaderTest.cs
@@ -184,8 +184,6 @@ namespace Cube.FileSystem.Tests
                     Assert.That(info.LastWriteTime,  Is.Not.EqualTo(DateTime.MinValue));
                     Assert.That(info.LastAccessTime, Is.Not.EqualTo(DateTime.MinValue));
                 }
-
-                Assert.That(bytes, Is.AtLeast(1));
             }
         }
 
