bd69b68723a74c453442228c9867a6157cabd8dd
clown <tt.clown@gmail.com>
2019-04-02 16:52:19 +0900

    Fix project settings.

--- Applications/Ice/Core/Sources/Settings/SettingsFolder.cs
+++ Applications/Ice/Core/Sources/Settings/SettingsFolder.cs
@@ -88,13 +88,9 @@ namespace Cube.FileSystem.SevenZip.Ice
             try
             {
                 if (Value == null) return;
-
-                var name = "cubeice-checker";
-                var dir  = IO.Get(Assembly.Location).DirectoryName;
-
-                new Startup(name)
+                new Startup("cubeice-checker")
                 {
-                    Command = IO.Combine(dir, $"{name}.exe").Quote(),
+                    Command = IO.Combine(Assembly.DirectoryName, "CubeChecker.exe").Quote(),
                     Enabled = Value.CheckUpdate,
                 }.Save();
             }
